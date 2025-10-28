a39d45882985cf8ad945254e39086a5570ba74cc
ibafaye7 <iba.faye7@gmail.com>
2025-10-28 21:57:25 +0100

    add Cross origin support

--- src/main/java/net/ifaye/ecommerce/dao/IProductRepository.java
+++ src/main/java/net/ifaye/ecommerce/dao/IProductRepository.java
@@ -3,7 +3,9 @@ package net.ifaye.ecommerce.dao;
 import net.ifaye.ecommerce.entities.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @RepositoryRestResource(collectionResourceRel = "product", path = "products")
+@CrossOrigin
 public interface IProductRepository extends JpaRepository<Product,Long> {
 }
