2803a6332c2ddab2894c3875969e69a5083457a3
ibafaye7 <iba.faye7@gmail.com>
2025-10-31 01:03:18 +0100

    update IProductRepository.java to find Products by category ID

--- src/main/java/net/ifaye/ecommerce/dao/IProductRepository.java
+++ src/main/java/net/ifaye/ecommerce/dao/IProductRepository.java
@@ -1,11 +1,15 @@
 package net.ifaye.ecommerce.dao;
 
 import net.ifaye.ecommerce.entities.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
 @RepositoryRestResource(collectionResourceRel = "product", path = "products")
 @CrossOrigin
 public interface IProductRepository extends JpaRepository<Product,Long> {
+    Page<Product> findByCategoryId(@Param("id") Long id, Pageable pageable);
 }
